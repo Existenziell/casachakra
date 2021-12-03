cd1c1d661aed159f096d024c5b94d740166bd983
Christof <bauer.christof@gmail.com>
2021-12-03 10:38:08 -0500

    Rework Nav + React Image Gallery

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,9 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Link from 'next/link'
 
 const Nav = () => {
 
-    const router = useRouter()
     const links = [
         { name: "Home", url: "/" },
         { name: "Retreats", url: "/retreats" },
@@ -12,21 +12,70 @@ const Nav = () => {
         { name: "Video", url: "/video" },
         { name: "Map", url: "/map" },
     ]
+    const router = useRouter()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const intercept = (e) => {
+        e.preventDefault()
+        setIsOpen(false)
+        router.push(e.target.href)
+    }
 
     return (
-        <ul className="flex justify-between my-4 ml-4 w-max space-x-8">
-            {links.map((l) => {
-                return (
-                    <li key={l.name}>
-                        <Link href={l.url}>
-                            <a className={`${router.pathname === l.url ? 'active-nav' : ''} hover:bg-brand hover:text-white transition-all block p-4`}>
-                                {l.name}
-                            </a>
-                        </Link>
-                    </li>
-                )
-            })}
-        </ul>
+        <>
+            {/* Desktop menu */}
+            <div className='dark:bg-black dark:text-gray-300 w-full py-3 md:py-0'>
+                <ul className="hidden md:flex justify-between items-center ml-4 w-max ">
+                    {links.map(l => (
+                        <li key={l.name}>
+                            <Link href={l.url}>
+                                <a
+                                    href={l.url}
+                                    className={`${router.pathname === l.url ? 'active-nav' : 'border-b-2 border-white dark:border-brand'} hover:bg-brand hover:text-white transition-all p-4 block`}>
+
+                                    {l.name}
+                                </a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Mobile menu */}
+                {isOpen &&
+                    <ul className="mobile-menu md:hidden absolute left-0 right-0 top-14 bg-white z-10 dark:bg-black dark:text-gray-300">
+                        {links.map(l => (
+                            <li key={l.name}>
+                                <a
+                                    href={l.url}
+                                    onClick={intercept}
+                                    className={`${router.pathname === l.url && 'active-nav'} w-full block text-base px-8 py-2 hover:bg-brand hover:text-white transition-all`}>
+
+                                    {l.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                }
+
+                {/* Mobile Hamburger Button */}
+                <div className="flex md:hidden justify-between ml-8 w-max space-x-6">
+                    <button className="mobile-menu-button outline-none" onClick={() => setIsOpen(!isOpen)} aria-label="Open Mobile Navigation">
+                        {!isOpen ?
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 hover:text-brand text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 hover:text-brand text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        }
+                    </button>
+                </div>
+
+            </div>
+
+        </>
+
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Layout from '../components/Layout'
 import Image from 'next/image'
+import ImageGallery from 'react-image-gallery'
+import * as Css from '../node_modules/react-image-gallery/styles/css/image-gallery.css'
 
 export default function Index() {
+
+  const images = [
+    {
+      original: '/yoga/miel1.jpg',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: '/yoga/miel2.jpg',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: '/yoga/miel3.jpg',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
+
   return (
     <div className='flex flex-col items-center justify-center gap-8' >
       <h1 className="text-4xl mb-8">Welcome to Casa Chakra</h1>
       <p className='mb-8'>Casa Chakras is a homey place with plenty of space for each one to have their privacy. Trees, flowers, and animals such as birds and iguanas are part of the place.
         %The rooms are simple yet comfortable in the morning sunlight comes through the window.
       </p>
-      <Image src="/yoga/miel1.jpg" width={1000} height={750} alt="Miel" />
-      <Image src="/yoga/miel2.jpg" width={1000} height={667} alt="Miel2" />
-      <Image src="/yoga/miel3.jpg" width={1000} height={654} alt="Miel3" />
+      <ImageGallery items={images} />
     </div>
   )
 }
